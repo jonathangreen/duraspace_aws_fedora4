6b50a3a0c9d62baa313d29c19f46fce1b34968e7
Jonathan Green <razzard@gmail.com>
2014-08-16 14:42:46 -0300

    Start of some puppets

---
+++ puppet/manifests/init.pp
@@ -0,0 +1,23 @@
+package { 'tomcat7':
+	ensure => present,
+}
+
+package { 'ec2-api-tools':
+	ensure => present,
+}
+
+package { 'fail2ban':
+	ensure => present,
+}
+
+package { 'unattended-upgrades':
+	ensure => present,
+}
+
+file {'tomcat-authbind-config':
+  	path    => '/etc/authbind/byport/80',
+  	ensure  => file,
+  	mode    => 500,
+  	owner => 'tomcat7',
+  	notify => Service['tomcat7']
+}
