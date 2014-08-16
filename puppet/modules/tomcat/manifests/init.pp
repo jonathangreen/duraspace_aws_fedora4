a84682c62c3a9843b71f41d97d8b6870092d9991
Jonathan Green <razzard@gmail.com>
2014-08-16 15:44:39 -0300

    More puppet updates

--- puppet/modules/tomcat/manifests/init.pp
+++ puppet/modules/tomcat/manifests/init.pp
@@ -6,28 +6,33 @@ class tomcat {
   }
 
   file {'tomcat-authbind-config':
-    	path    => '/etc/authbind/byport/80',
-    	ensure  => file,
-    	mode    => 500,
-    	owner 	=> 'tomcat7',
-      require => Package['tomcat7'],
-    	notify 	=> Service['tomcat7'],
+  	path    => '/etc/authbind/byport/80',
+  	ensure  => file,
+  	mode    => 500,
+  	owner 	=> 'tomcat7',
+    group   => 'tomcat7',
+    require => Package['tomcat7'],
+  	notify 	=> Service['tomcat7'],
   }
 
   file {'tomcat-defaults' :
   	path	=> '/etc/default/tomcat7',
   	ensure	=> file,
   	source	=> 'puppet:///modules/tomcat/tomcat7',
-      require => Package['tomcat7'],
-    	notify 	=> Service['tomcat7'],
+    owner   => 'tomcat7',
+    group   => 'tomcat7',
+    require => Package['tomcat7'],
+    notify 	=> Service['tomcat7'],
   }
 
   file {'tomcat-server-xml' :
   	path	=> '/etc/tomcat7/server.xml',
   	ensure	=> file,
   	source	=> 'puppet:///modules/tomcat/server.xml',
-      require => Package['tomcat7'],
-    	notify 	=> Service['tomcat7'],
+    owner   => 'tomcat7',
+    group   => 'tomcat7',
+    require => Package['tomcat7'],
+    notify 	=> Service['tomcat7'],
   }
 
   package { 'tomcat7':
