92ce2caeb785af521c76a6ffa28b24442cb39e74
Jonathan Green <razzard@gmail.com>
2014-08-16 14:47:46 -0300

    More puppet updates

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -14,11 +14,16 @@ package { 'unattended-upgrades':
 	ensure => present,
 }
 
+service { 'tomcat7':
+  ensure    => running,
+  enable    => true,
+  require => package['tomcat7'],
+}
+
 file {'tomcat-authbind-config':
   	path    => '/etc/authbind/byport/80',
   	ensure  => file,
   	mode    => 500,
   	owner => 'tomcat7',
-  	notify => Service['tomcat7'],
-  	require => Package['tomcat7'],
+  	notify => service['tomcat7'],
 }
